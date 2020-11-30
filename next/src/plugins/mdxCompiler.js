51c05936774ea91bbb7bbc22a8e4aa440df85933
Jared Palmer <jared@palmer.net>
2020-11-30 11:48:23 -0500

    Render the content of PlatformContent

--- next/src/plugins/mdxCompiler.js
+++ next/src/plugins/mdxCompiler.js
@@ -13,16 +13,26 @@ const parse = (mdx) => {
   return ast;
 };
 
+/**
+ * A custom remark mdx plugin that adds support for a `<PlatformContent includePath=""/>`
+ * component. This component will look for a file in the `includes` directory that matches
+ * the `opts.platform` string (usually determined by a url).
+ *
+ * @param {*} opts options (platform and frontmatter)
+ */
 module.exports = (opts = {}) => (tree) => {
   visit(tree, "mdxBlockElement", async (node, index, parent) => {
     if (node.name === "PlatformContent") {
       // console.log("opts", opts);
 
       const { value } = node.attributes.find((a) => a.name === "includePath");
-
+      // console.log(value);
       if (!value) {
         return;
       }
+
+      // Lookup and then read the file in the includes directory
+      // and potentially also the fallback given the value of the `includePath` prop
       const includesPath = path.join(
         process.cwd(),
         "src",
@@ -40,17 +50,19 @@ module.exports = (opts = {}) => (tree) => {
       let raw;
       const includesExists = fs.existsSync(includesPath);
       if (includesExists) {
-        raw = fs.readFileSync(includesPath);
+        raw = fs.readFileSync(includesPath, "utf8");
       } else if (fs.existsSync(fallbackPath)) {
-        raw = fs.readFileSync(fallbackPath);
+        raw = fs.readFileSync(fallbackPath, "utf8");
       }
       // console.log("path", contentPath);
-
       // console.log("raw", raw);
+
+      // Now we parse the includes as MDX again, and then stitch the
+      // AST so that we actually render the <PlatformContent>
+      // component as well.
       if (raw) {
-        node.children = parse(raw);
+        node.children = parse(raw).children;
       }
-      // parent.children.splice(index, 1, ...newNode);
     }
   });
 
