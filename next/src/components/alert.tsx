d1d0687cf85f1bcd634a7a6dbb0bb076606ed5ce
Jared Palmer <jared@palmer.net>
2020-12-15 12:47:18 -0500

    Add alert component

--- next/src/components/alert.tsx
+++ next/src/components/alert.tsx
@@ -1,11 +1,41 @@
-import * as React from "react";
+import React from "react";
 
-export interface AlertProps {
-  children: React.ReactNode;
-}
+type Props = {
+  title?: string;
+  level?: string;
+  deepLink?: string;
+  dismiss?: boolean;
+  children?: any;
+};
 
-export default function Alert(props: AlertProps) {
-  return <div className="Alert">{props.children}</div>;
-}
-
-Alert.displayName = "Alert";
+export default ({
+  title,
+  children,
+  level,
+  deepLink,
+  dismiss = false,
+}: Props): JSX.Element => {
+  let className = "alert";
+  if (level) {
+    className += ` alert-${level}`;
+  }
+  if (children.props && typeof children.props.children === "string") {
+    className += " markdown-text-only";
+  }
+  return (
+    <div className={className} role="alert" id={deepLink}>
+      {dismiss && (
+        <button
+          type="button"
+          className="close"
+          data-dismiss="alert"
+          aria-label="Close"
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
+      )}
+      {title && <h5 className="no_toc">{title}</h5>}
+      <div className="alert-body content-flush-bottom">{children}</div>
+    </div>
+  );
+};
